8be8412fd8b086bb122aaaa9b66c4a14fe7be294
Tran Nguyen Ba Phuoc <nguyenbaphuoctran@gmail.com>
2017-01-31 18:29:42 +0900

    Finished

---
+++ SQLDependency/VM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLDependency
+{
+    class VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public List<Conversation_Detail> LstData { get; set; }
+        private clsNotification notification;
+        public VM()
+        {
+            // Initialize instance
+            string command = @"select [CD_ID],[Content] from [dbo].[Conversation_Detail]";
+            notification = new clsNotification(setData, command);
+            // Start listening notification
+            notification.loadData();
+        }
+
+        // setData() will run when notification.loadData() called, so you do not need to call.
+        private void setData() 
+        {
+            App.Current.Dispatcher.Invoke(delegate
+            {
+                SQLDependency_ExampleEntities en = new SQLDependency_ExampleEntities();
+                LstData = en.Conversation_Detail.ToList();
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LstData"));
+            });
+        }
+
+        private void On_Quit()
+        {
+            notification.Dispose();
+        }
+    }
+}
---
+++ SQLDependency/clsNotification.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLDependency
+{
+   class clsNotification:IDisposable
+    {
+        private string connectionString = @"data source=.\SQLEXPRESS;initial catalog=SQLDependency_Example;integrated security=True;";
+        private string command;
+        private Action Act;
+        public clsNotification(Action obj,string cmd)
+        {
+            Act = obj;
+            command = cmd;
+            // Start listening notification
+            SqlDependency.Start(connectionString); 
+        }
+
+        public void loadData()
+        {
+            // Connect to DB, create subscriber
+            using (SqlConnection con = new SqlConnection(connectionString)) 
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand(command, con);
+                SqlDependency de = new SqlDependency(cmd);
+
+                // This event will run when receive message from DB
+                de.OnChange += new OnChangeEventHandler(de_OnChange);
+
+                // Subcriber
+                cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+                // Run action what you want to do on changed
+                Act?.Invoke();
+            }
+        }
+
+        private void de_OnChange(object sender, SqlNotificationEventArgs e)
+        {
+            SqlDependency de = sender as SqlDependency;
+            de.OnChange -= de_OnChange;
+            loadData();
+        }
+
+        public void Dispose()
+        {
+            // Unregister the notification subscription for the current instance.
+            SqlDependency.Stop(connectionString); 
+        }
+    }
+}
